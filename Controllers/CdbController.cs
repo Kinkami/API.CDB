52e702702e530c7173797be1c402025bc5a71f9b
Fernando <fernando.henrique.leme@gmail.com>
2024-07-05 17:40:14 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/CdbController.cs
@@ -0,0 +1,48 @@
+﻿using API.Cdb.Domain.Dto;
+using API.Cdb.Domain.Interfaces.Business;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.CDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CdbController : ControllerBase
+    {
+        private readonly ICdbBusiness _cdbBusiness;
+
+        public CdbController(ICdbBusiness cdbBusiness)
+        {
+            _cdbBusiness = cdbBusiness;
+        }
+
+        /// <summary>
+        /// Calcular CDB
+        /// </summary>
+        /// <returns>retorna o valor bruto e líquido</returns>
+        [HttpGet("calcularCdb")]
+        [EnableCors()]
+        public ActionResult CalcularCdb([FromQuery][Required] CalcularCdbRequestDto calcularCdbRequest)
+        {
+            try
+            {
+                var result = _cdbBusiness.CalcularCdb(calcularCdbRequest);
+                return Ok(result);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return Problem(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+using API.Cdb.IoC;
+using Microsoft.OpenApi.Models;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+DependencyInjectionConfig.RegisterServices(builder.Services);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API Cálculo CDB", Version = "v1" });
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Cálculo CDB V1");
+    });
+}
+
+app.UseCors("AllowAll");
+
+app.Run();
